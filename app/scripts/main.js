39931184eb36b5cca6f396996342f9688b28341c
SA\TB197DN <fabianslu2@gmail.com>
2017-05-20 22:54:45 -0600

    adding comments to the code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,7 @@
   var CURRENCY = '$';
   var PROGRESS_BAR_COLOR = '#61b256';
 
+  // template to reset the progress bar animation
   var proresBarHTML = '<div id="progress-bar" class="barfiller">' + 
     '<div class="tipWrap">' +
     '<span class="tip"></span>' +
@@ -17,11 +18,12 @@
   // setting popover options
   var progressPopoverOptions = {
     height: 140,
-    title: 'Your progress',
     width: 580,
+    title: 'Your progress',
     onShow: onShow
   };
 
+  // setting progress bar options
   var progressBarOptions = {
     // color of bar
     barColor: PROGRESS_BAR_COLOR,
@@ -36,24 +38,32 @@
     animateOnResize: true,
 
     // custom text to show
-    textToShow: "$" + INITIAL_VALUE,
+    textToShow: CURRENCY + INITIAL_VALUE,
 
     // percentage to show
     percentage: (INITIAL_VALUE * 100) / TARGET // calculating percentage
   };
 
+  /**
+   * funciton called by the popover plugin when the popover is shown
+   * @param  {Object} $element jQuery element of the popover
+   * @return {null}          
+   */
   function onShow($element) {
+    // resetting progress bar html
     $element.find('#progress-bar-container').html(proresBarHTML);
+    // creating progress bar animation
     $element.find('#progress-bar').barfiller(progressBarOptions);
   }
 
-  
+  // finding and setting the target text to display it
   $('#target').text(CURRENCY + TARGET);
+
+  // calculating the remaining and setting it as text
   $('#remaining').text(CURRENCY + (TARGET - INITIAL_VALUE));
+
   // linking popover to link
   $('#progress-link').webuiPopover(progressPopoverOptions);
 
   
-
-
 })(jQuery, window, document)
